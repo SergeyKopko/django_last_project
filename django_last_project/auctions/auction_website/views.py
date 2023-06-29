6e2af5972bf7270c1f0cfaa53dc8a636144b3fde
SergeyKopko <133692248+SergeyKopko@users.noreply.github.com>
2023-06-29 19:47:49 +0300

    Add files via upload

--- django_last_project/auctions/auction_website/views.py
+++ django_last_project/auctions/auction_website/views.py
@@ -3,7 +3,7 @@ from .views import *
 
 
 def main_window(request):
-    return HttpResponse('Главная страница')
+    return HttpResponse('Главная страница ВОТ ТАК')
 # Create your views here.
 def auction_cabinet(request):
     return HttpResponse('Кабинет пользователя')
